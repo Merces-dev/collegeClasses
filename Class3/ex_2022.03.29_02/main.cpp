3c1091d1d5e576560057f34bba38244fed023f35
Merces-dev <giovani.merces@intelitrader.com.br>
2022-04-06 15:18:29 -0300

    Adjust code indentation  in the 3° class.

--- Class3/ex_2022.03.29_02/main.cpp
+++ Class3/ex_2022.03.29_02/main.cpp
@@ -20,14 +20,14 @@ int main()
         }
     };
 
-        //Caso a terceiro (0, 1, 2, 4) seja maior que 1000 o mesmo deve ser interrompido e o código deve parar;
-        if(quad[3] > 1000){
-            printf("%.2f ² = %.2f", notas[3], quad[3]);
-        }else{
-            for(int i = 1; i < 5; i++){
-                printf("\n%.2f ² = %.2f", notas[i], quad[i]);
-            }
-        };
+    //Caso a terceiro (0, 1, 2, 4) seja maior que 1000 o mesmo deve ser interrompido e o código deve parar;
+    if(quad[3] > 1000){
+        printf("%.2f ² = %.2f", notas[3], quad[3]);
+    }else{
+        for(int i = 1; i < 5; i++){
+        	printf("\n%.2f ² = %.2f", notas[i], quad[i]);
+        }
+    };
 
 
     return 0;
